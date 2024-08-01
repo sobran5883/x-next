744c589cdfcc4e2083e793f47e5a96fd17cd1bb6
sobran rajak <sobran0621@gmail.com>
2024-08-01 23:08:56 +0530

    adding next-auth

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,13 @@
+"use client"
+
+import {signIn, signOut, useSession} from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
 import {FaXTwitter} from "react-icons/fa6"
 import {HiHome} from "react-icons/hi"
+
 export default function Sidebar() {
+  const {data: session} = useSession();
   return (
     <div className='flex flex-col gap-4 p-3'>
         <Link href="/">
@@ -12,7 +17,13 @@ export default function Sidebar() {
             <HiHome className='w-7 h-7 '/>
             <span className='font-bold hidden xl:inline'>Home</span>
         </Link>
-        <button className='bg-blue-400 text-white rounded-full hover:bg-blue-600 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>Sign In</button>
+        {session ? (
+          <button onClick={()=>signOut()} className='bg-blue-400 text-white rounded-full hover:bg-blue-600 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>Sign Out</button>
+        ):(
+          <button onClick={()=>signIn()} className='bg-blue-400 text-white rounded-full hover:bg-blue-600 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline font-semibold'>Sign In</button>
+        )}
+        
+        
     </div>
   )
 }
