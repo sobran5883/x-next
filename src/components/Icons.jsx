621ff142675cb9debbfd6ee1b4e398052133119a
sobran rajak <sobran0621@gmail.com>
2024-08-05 22:11:05 +0530

    install and implementing recoil

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -5,11 +5,14 @@ import { signIn, useSession } from 'next-auth/react';
 import { collection, deleteDoc, doc, getFirestore, onSnapshot, serverTimestamp, setDoc } from 'firebase/firestore';
 import {app} from '../firebase';
 import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { modalState } from '@/atom/modalAtom';
 
 export default function Icon({id, uid}) {
   const {data:session} = useSession();
   const [isLiked, setIsLiked] = useState(false);
   const [likes, setLikes] = useState([]);
+  const [open, setOpen] = useRecoilState(modalState);
   const db=getFirestore(app);
   const likePost= async()=>{
     if(session){
@@ -56,7 +59,9 @@ export default function Icon({id, uid}) {
 
   return (
     <div className='flex justify-start gap-5 p-2 text-gray-500'>
-      <HiOutlineChat className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100'/>
+      <HiOutlineChat className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100'
+       onClick={()=>setOpen(!open)}
+      />
       <div className='flex items-center'>
         {isLiked?(
           <HiHeart onClick={likePost} className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100'/>
