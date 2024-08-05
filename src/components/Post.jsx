cca68e2f03dad5a8817f692c45f353c591c28ce6
sobran rajak <sobran0621@gmail.com>
2024-08-05 20:42:42 +0530

    update icons component to handle likes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -9,7 +9,7 @@ export default function Post({post, id}) {
       <div className='flex-1'>
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-1 whitespace-nowrap">
-            <h4 className="font-bold text-sx truncate">{post?.name}</h4>
+            <h4 className="font-bold text-sm truncate">{post?.name}</h4>
             <span className="text-sm truncate">@{post?.username}</span>
           </div>
           <HiDotsHorizontal className="text-sm"/>
@@ -20,7 +20,7 @@ export default function Post({post, id}) {
         <Link href={`/posts/${id}`}>
           <img src={post?.image} alt="post-img" className="rounded-2xl mr-2"/>
         </Link>
-        <Icons/>
+        <Icons id={id}/>
       </div>
     </div>
   )
