2f7b5f500b926973df7dc655ac00a4a00e5d6a87
sobran rajak <sobran0621@gmail.com>
2024-08-08 19:50:44 +0530

    adding comment functionality

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -6,13 +6,15 @@ import { collection, deleteDoc, doc, getFirestore, onSnapshot, serverTimestamp, 
 import {app} from '../firebase';
 import { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
-import { modalState } from '@/atom/modalAtom';
+import { modalState, postIdState } from '@/atom/modalAtom';
 
 export default function Icon({id, uid}) {
   const {data:session} = useSession();
   const [isLiked, setIsLiked] = useState(false);
   const [likes, setLikes] = useState([]);
   const [open, setOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
+  const [comments, setComments] = useState([]);
   const db=getFirestore(app);
   const likePost= async()=>{
     if(session){
@@ -41,6 +43,13 @@ export default function Icon({id, uid}) {
     setIsLiked(likes.findIndex((like)=>like.id===session?.user?.uid)!==-1);
   },[likes]);
 
+  useEffect(()=>{
+    const unsubscribe = onSnapshot(collection(db, 'posts', id, 'comments'),
+    (snapshot)=>setComments(snapshot.docs)
+    );
+    return ()=> unsubscribe();
+  }, [db, id]);
+
   const deletePost = async ()=>{
     if(window.confirm('Are you sure you want to delete this post?')){
       if(session?.user?.uid === uid){
@@ -59,9 +68,24 @@ export default function Icon({id, uid}) {
 
   return (
     <div className='flex justify-start gap-5 p-2 text-gray-500'>
+      <div className='flex items-center'>
       <HiOutlineChat className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100'
-       onClick={()=>setOpen(!open)}
+       onClick={()=>{
+        if(!session){
+          signIn();
+        }
+        else{
+          setOpen(!open)
+          setPostId(id);
+        }
+       }}
       />
+      {
+        comments.length > 0 && (
+          <span className='text-xs'>{comments.length}</span>
+        )
+      }
+      </div>
       <div className='flex items-center'>
         {isLiked?(
           <HiHeart onClick={likePost} className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 text-red-600 hover:text-red-500 hover:bg-red-100'/>
